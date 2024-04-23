ec2736ffeeae471c33bb8bf9c46149d93341f408
Matias Navio <naviomatias2@gmail.com>
2024-04-23 10:38:45 -0300

    Initial commit

---
+++ src/main/java/com/matias/springboot/app/security/springbootsecurity/SpringbootSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.matias.springboot.app.security.springbootsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matias/springboot/app/security/springbootsecurity/SpringbootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matias.springboot.app.security.springbootsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
