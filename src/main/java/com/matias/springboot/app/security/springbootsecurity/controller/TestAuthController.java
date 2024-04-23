6e5d966641804c89f459982a2b8326baa9fda132
Matias Navio <naviomatias2@gmail.com>
2024-04-23 15:04:45 -0300

    Creando las entidades para la relación obtienedo de la DB

--- src/main/java/com/matias/springboot/app/security/springbootsecurity/controller/TestAuthController.java
+++ src/main/java/com/matias/springboot/app/security/springbootsecurity/controller/TestAuthController.java
@@ -1,8 +1,30 @@
 package com.matias.springboot.app.security.springbootsecurity.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/auth")
+@PreAuthorize("denyAll()")
 public class TestAuthController {
     
+    @GetMapping("/hello")
+    @PreAuthorize("permitAll()")
+    public String hello(){
+        return "Hola mundo";
+    }
+
+    @GetMapping("/hello-secured")
+    @PreAuthorize("hasAnyAuthority('READ')")
+    public String helloSecured(){
+        return "Hola mundo secured";
+    }
+
+    @GetMapping("/hello-secured2")
+    @PreAuthorize("hasAnyAuthority('CREATE')")
+    public String helloSecured2(){
+        return "Hola mundo secured2";
+    }
 }
